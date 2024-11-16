4a5bbc7dc427032592fbdc041f7159fd08fd62bc
Camilo Zuliaga <v-czuliaga@youngliving.com>
2024-11-16 08:16:53 -0500

    Initi Commit, Entity created

---
+++ ToDo.Domain/Primitives/Entity.cs
@@ -0,0 +1,44 @@
+﻿namespace ToDo.Domain.Primitives
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        protected Entity(long id) => Id = id;
+
+        protected Entity()
+        {
+        }
+
+        public long Id { get; private init; }
+
+        public static bool operator ==(Entity? left, Entity? right) =>
+            left is not null && right is not null && left.Equals(right);
+
+        public static bool operator !=(Entity? left, Entity? right) =>
+            !(left == right);
+
+        public bool Equals(Entity? other)
+        {
+            if (other is null ||
+               other.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return other.Id == Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null ||
+                obj.GetType() != GetType() ||
+                obj is not Entity entity)
+            {
+                return false;
+            }
+
+            return entity.Id == Id;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode() * 30;
+    }
+}
